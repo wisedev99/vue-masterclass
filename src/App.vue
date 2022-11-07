45e3cde2e673b7255e6ec6a0c07bcf0a39b1b517
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2022-11-07 11:18:59 +0500

    add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
+  <the-navbar />
   <div class="">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
+import TheNavbar from '@/components/TheNavbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TheNavbar
+  }
 }
 </script>
 
