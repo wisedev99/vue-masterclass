9c1e111131cb7327d987c1f86a04489f7af4a66f
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2022-11-07 11:33:21 +0500

    fetching authenticated user from Vuex

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,27 +1,24 @@
 <template>
   <header class="header" id="header">
-
     <router-link :to="{name: 'Home'}" class="logo">
       <img src="../assets/images/vueschool-logo.svg">
     </router-link>
-
     <div class="btn-hamburger">
       <!-- use .btn-humburger-active to open the menu -->
       <div class="top bar"></div>
       <div class="middle bar"></div>
       <div class="bottom bar"></div>
     </div>
-
     <!-- use .navbar-open to open nav -->
     <nav class="navbar">
       <ul>
         <li class="navbar-user">
           <a href="#">
-            <img class="avatar-small" src="https://pbs.twimg.com/profile_images/1188775562657091594/5mgkg44t_400x400.jpg" alt="">
+            <img class="avatar-small" :src="authUser.avatar" :alt="`${authUser.name} profile picture`"/>
             <span>
-                        Alex Kyriakidis
-                        <img class="icon-profile" src="../assets/images/arrow-profile.svg" alt="">
-                    </span>
+                {{authUser.name}}
+                <img class="icon-profile" src="../assets/images/arrow-profile.svg" alt=""/>
+            </span>
           </a>
 
           <!-- dropdown menu -->
@@ -35,7 +32,6 @@
           </div>
         </li>
       </ul>
-
 <!--      <ul>-->
 <!--        <li class="navbar-item">-->
 <!--          <a href="index.html">Home</a>-->
@@ -62,7 +58,12 @@
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['authUser'])
+  }
+}
 </script>
 
 <style scoped>
